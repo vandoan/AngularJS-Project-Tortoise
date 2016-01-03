4cbf2f215ad89495f91555bd4f268a4cb66ae105
Van Doan <vdoan09@gmail.com>
2016-01-02 23:51:11 -0500

    chapter 4 and testing

---
+++ Chapter 1-3/controller.js
@@ -0,0 +1,18 @@
+// File: chapeter3/ controller.js  -  page 44
+
+angular.module('notesApp',[])
+	.controller('ListCtrl', [function() {
+
+		var self = this;
+		self.items = [
+			{ id: 1, 	label: 'First', 	done: true 	},
+			{ id: 2, 	label: 'Second', 	done: false }
+		];
+
+		self.getDoneClass = function(item) {
+			return {
+				finished: item.done,
+				unfinished: !item.done,
+			};
+		};
+	}])
